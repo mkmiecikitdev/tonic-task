5e06a102483f6439581e25126854541958f118e2
Bambz <mich.kmiecik@gmail.com>
2019-11-26 13:21:49 +0100

    init

---
+++ src/main/java/kmiecik/michal/tonictask/TonicTaskApplication.java
@@ -0,0 +1,13 @@
+package kmiecik.michal.tonictask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonicTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonicTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kmiecik/michal/tonictask/TonicTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package kmiecik.michal.tonictask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonicTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
